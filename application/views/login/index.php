66e288c4b7dc7f8ffd910e26f98b3fb98ed46d4c
spidi123q <spidi123q@gmail.com>
2017-03-02 22:26:28 +0530

    manifest file added

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -3,6 +3,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="manifest" href="manifest.json">
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/index.css">
 	<script type="text/javascript">
 						if ('serviceWorker' in navigator) {
