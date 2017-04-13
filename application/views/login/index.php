cd5ea1168535f5ad9f175f32e9f59d9746bf42f5
spidi123q <spidi123q@gmail.com>
2017-04-13 22:15:21 +0530

    google analytic traking added

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -7,6 +7,16 @@
 	<link rel="icon" href="https://thoughtifies.com/images/fav.png">
 	<link rel="manifest" href="https://thoughtifies.com/manifest.json">
 	<link rel="stylesheet" href="https://thoughtifies.com/css/index.css">
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-97318105-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 	<!--
 	<script type="text/javascript">
 						if ('serviceWorker' in navigator) {
