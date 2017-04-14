5c34b6ca5943bb1ab1987e50e843d93da8e9bac0
spidi123q <spidi123q@gmail.com>
2017-04-14 14:00:00 +0530

    merge

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -7,16 +7,6 @@
 	<link rel="icon" href="https://thoughtifies.com/images/fav.png">
 	<link rel="manifest" href="https://thoughtifies.com/manifest.json">
 	<link rel="stylesheet" href="https://thoughtifies.com/css/index.css">
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-97318105-1', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
 	<!--
 	<script type="text/javascript">
 						if ('serviceWorker' in navigator) {
@@ -36,8 +26,7 @@
 </head>
 	<body class="login_body" >
 		<div  class="title flex-container" style="">
-			<h4>Alpha testing...will open soon</h4>
-							<form class="hide" name="loginform" action="data/1" method="post" >
+							<form class="" name="loginform" action="data/1" method="post" >
 									<div class="container-fluid row-fluid">
 										<div id="fdf" class="col-sm-7">L social network</div>
 												<div class="col-sm-2">
